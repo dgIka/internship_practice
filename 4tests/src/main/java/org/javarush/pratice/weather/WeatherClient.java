9f3919f176544e3c16803a90e68c5f8abc305e22
Ika <rexar.ika@gmail.com>
2025-06-20 16:21:38 +0300

    cookies

--- 4tests/src/main/java/org/javarush/pratice/weather/WeatherClient.java
+++ 4tests/src/main/java/org/javarush/pratice/weather/WeatherClient.java
@@ -3,19 +3,28 @@ package org.javarush.pratice.weather;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 
+import java.net.CookieManager;
+import java.net.HttpCookie;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.sql.SQLOutput;
 import java.time.Duration;
+import java.util.List;
 
 public class WeatherClient {
     private static final String API_KEY = "b634f3ff5e0c4ad094f101348251806";
     private static final String CITY = "Moscow";
 
+    private static CookieManager cookieManager = new CookieManager();
+
     public static void main(String[] args) {
-        HttpClient client = HttpClient.newHttpClient();
+        HttpClient client = HttpClient.newBuilder()
+                .version(HttpClient.Version.HTTP_2)
+                .connectTimeout(Duration.ofSeconds(10))
+                .cookieHandler(cookieManager)
+                .build();
 
         String apiURI = "http://api.weatherapi.com/v1/current.json?key="
                 + API_KEY + "&q="
@@ -54,6 +63,15 @@ public class WeatherClient {
                         System.out.println("Текущая погода в " + CITY + " " + country + ": ");
                         System.out.println("Температура: " + tempC);
                         System.out.println("Облачность: " + condition);
+
+                        List<HttpCookie> cookies = cookieManager.getCookieStore().getCookies();
+                        if (cookies.isEmpty()) {
+                            System.out.println("Куки отсутствуют");
+                        } else {
+                            for (HttpCookie cookie : cookies) {
+                                System.out.println("Cookie: " + cookie.getName() + ": " + cookie.getValue());
+                            }
+                        }
 
 
                     } catch (Exception e) {
